911008e57b0c882836076d99fcb3e38360937752
dpr5 <meshie75@gmail.com>
2018-05-11 11:32:46 -0400

    Cards and Adapter added

---
+++ app/src/main/java/com/dreaminreality/ranad_000/pokemunity/MyData.java
@@ -0,0 +1,13 @@
+package com.dreaminreality.ranad_000.pokemunity;
+
+public class MyData {
+
+    static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich","JellyBean", "Kitkat", "Lollipop", "Marshmallow"};
+    static String[] versionArray = {"1.5", "1.6", "2.0-2.1", "2.2-2.2.3", "2.3-2.3.7", "3.0-3.2.6", "4.0-4.0.4", "4.1-4.3.1", "4.4-4.4.4", "5.0-5.1.1","6.0-6.0.1"};
+
+    static Integer[] drawableArray = {R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground, R.drawable.common_google_signin_btn_icon_dark,
+            R.drawable.common_google_signin_btn_icon_dark, R.drawable.common_full_open_on_phone, R.drawable.common_google_signin_btn_icon_dark_normal, R.drawable.fui_idp_button_background_google,
+            R.drawable.googleg_standard_color_18, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
