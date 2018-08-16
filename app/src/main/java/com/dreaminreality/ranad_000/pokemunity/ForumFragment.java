0ce4b6375197aa4157c82e5bf9bc5e1502d7ba3a
dpr5 <meshie75@gmail.com>
2018-08-16 10:00:34 -0400

    Forum Fragment Started

--- app/src/main/java/com/dreaminreality/ranad_000/pokemunity/ForumFragment.java
+++ app/src/main/java/com/dreaminreality/ranad_000/pokemunity/ForumFragment.java
@@ -7,21 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link PokedexFragment.OnFragmentInteractionListener} interface
+ * {@link ForumFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link PokedexFragment#newInstance} factory method to
+ * Use the {@link ForumFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
- *
  */
-
-public class PokedexFragment extends Fragment {
+public class ForumFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,11 +26,10 @@ public class PokedexFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    Button exit_button;
 
     private OnFragmentInteractionListener mListener;
 
-    public PokedexFragment() {
+    public ForumFragment() {
         // Required empty public constructor
     }
 
@@ -44,11 +39,11 @@ public class PokedexFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PokedexFragment.
+     * @return A new instance of fragment ForumFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PokedexFragment newInstance(String param1, String param2) {
-        PokedexFragment fragment = new PokedexFragment();
+    public static ForumFragment newInstance(String param1, String param2) {
+        ForumFragment fragment = new ForumFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -69,7 +64,7 @@ public class PokedexFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokedex, container, false);
+        return inflater.inflate(R.layout.fragment_forum, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
